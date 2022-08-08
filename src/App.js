419db8251bd0a37bd503fee34f654bbe3f94cee0
Changraekang <storkcr@gmail.com>
2022-08-08 12:00:17 +0900

    헤더 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import Header from "./Header";
+
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
